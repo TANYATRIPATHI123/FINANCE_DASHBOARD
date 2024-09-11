26452a880bbc273ae5508f7ab5e20b7187726b1f
Tanya <7099.tanya@gmail.com>
2024-09-11 16:07:06 +0530

    Backend

---
+++ server/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js"
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
+
+// configuration
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy : "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(cors());
+app.use("/kpi",kpiRoutes);
+
+
+// mongoose setup
+const PORT = process.env.PORT || 9000;
+mongoose
+.connect(process.env.MONGO_URL , {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+})
+.then(async() => {
+    app.listen(PORT,() => console.log(`Server Port : ${PORT}`))
+    
+    /* ADD DATA ONLY ONE TIME OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase(); // to avoid duplicates
+    // KPI.insertMany(kpis);
+
+})
+.catch((error) => console.log(`${error} did not connect`));
+
+
+
+
